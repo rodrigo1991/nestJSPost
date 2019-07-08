6a9dd30c0c834282c6dd0b5c929805415d171c57
rodrigo <ro.rivero1991@gmail.com>
2019-07-08 17:55:20 -0400

    cambios

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,53 +9,38 @@ import {
 import { User } from './user.entity';
 
 @ApiBearerAuth()
-//@ApiUseTags('users')
+//@ApiUseTags('usuarios')
 @Controller('users')
 export class UserController {
 
-  constructor(private readonly userservice: UserService) {}
+  constructor(private readonly usuarioService: UserService) {}
 
   @Get()
-  find(): User[] {
-    try{
-    const salida = this.userservice.findAll();
-    console.log(`salida directa ${salida}`);
-    salida.then(valor => 
-      console.log(`salida then`, valor)
-    );
-    const retorno = await this.userservice.findAll();
-    return retorno;
-
-    //const salidawait = await this.userservice.findAll();
-    //console.log(salidawait);
-    //return salidawait;
-    }
-    catch(err){
-      console.log(err);
-    }
+  async find(): Promise<User[]> {
+    return await this.usuarioService.findAll();
   }
 
   @Get(':id')
   async findMe(@Param('id') id: number): Promise<User> {
-    return await this.userservice.findById(id);
+    return await this.usuarioService.findById(id);
   }
 
   @Put(':id')
   async update(@Param('id') id: number, @Body() usuario: User) {
     console.log(usuario);
-    return await this.userservice.update(id, usuario);
+    return await this.usuarioService.update(id, usuario);
   }
 
   @Post()
   async create(@Body() usuario: User) {
     console.log(usuario);
-    return this.userservice.create(usuario);
+    return this.usuarioService.create(usuario);
   }
 
   /*
-  @Delete('users/:slug')
+  @Delete('usuarios/:slug')
   async delete(@Param() params) {
-    return await this.userservice.delete(params.slug);
+    return await this.usuarioService.delete(params.slug);
   }*/
 
 }
